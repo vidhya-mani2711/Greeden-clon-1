52d44ea764892a80522e7dbf0dac16a8fe73e7a7
vidhya-mani2711 <vidhyavidhya975@gmail.com>
2025-05-19 21:12:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//selecting side navbar menubar
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+
+menuicon.addEventListener("click",function()
+{
+    sidenav.style.right=0
+
+
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+
